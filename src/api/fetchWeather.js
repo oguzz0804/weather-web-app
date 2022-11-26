656e738ae96499047ca491f4a138c9cf42e49b8a
Oğuzhan Ötleş <oguzz0804@gmail.com>
2022-11-26 22:40:13 +0300

    finished

---
+++ src/api/fetchWeather.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+const URL = 'https://api.openweathermap.org/data/2.5/weather'
+const API_KEY = '5f9f2289447d2b885ff25615cf81ba6a'
+
+const fetchWeather = async (query) => {
+    const { data } = await axios.get(URL, {
+        params: {
+            q: query,
+            units: 'metric',
+            APPID: API_KEY,
+        }
+    })
+
+    return data
+}
+
+export default fetchWeather
